3f5752c40abdae2cc2d7b6219343fbb99ca225af
Tim <tim@brightsidedeveloper.com>
2023-05-09 19:40:49 -0500

    Save

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const userSchema = new Schema(
   {
@@ -22,34 +23,43 @@ const userSchema = new Schema(
 )
 
 userSchema.statics.login = async function (username, email, password) {
+  //* Checks
   if (!((username || email) && password)) throw new Error('Missing fields')
-  const user = await this.findOne({ $or: [{ email }, { username }] })
 
+  //* Find and validate user
+  const user = await this.findOne({ $or: [{ email }, { username }] })
   if (!user) {
     if (email) throw new Error('Email does not exist')
     throw new Error('Username does not exist')
   }
-  console.log(user)
-  const match = await bcrypt.compare(password, user.password)
 
+  //* Check password
+  const match = await bcrypt.compare(password, user.password)
   if (!match) throw new Error('Password is incorrect')
 
   return user
 }
 
 userSchema.statics.signup = async function (username, email, password) {
+  //* Checks
   if (!username || !email || !password) throw new Error('Missing fields')
-  const exists = await this.findOne({ $or: [{ email }, { username }] })
+  if (!validator.isEmail(email)) throw new Error('Invalid email')
+  if (password.length < 6) throw new Error('Weak password')
 
+  //* Check if username/email exists
+  const exists = await this.findOne({ $or: [{ email }, { username }] })
   if (exists) {
     if (exists.email === email) throw new Error('Email already exists')
     throw new Error('Username already exists')
   }
 
+  //* Hash password
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
 
+  //* Create user
   const newUser = this.create({ username, email, password: hash })
+
   return newUser
 }
 
